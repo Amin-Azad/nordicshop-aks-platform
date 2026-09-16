e909a38037617ca0009bebcb9d3b2bf332157bdd
Amin Azad <azadamin079@gmail.com>
2026-09-16 13:27:24 +0200

    aks main

--- infra/modules/aks/main.tf
+++ infra/modules/aks/main.tf
@@ -26,6 +26,8 @@ resource "azurerm_kubernetes_cluster" "this" {
   network_profile {
     network_plugin      = "azure"
     network_plugin_mode = "overlay"
+    //network_data_plane  = "cilium"
+    //network_policy      = "cilium"
 
     pod_cidr       = var.pod_cidr
     service_cidr   = var.service_cidr
